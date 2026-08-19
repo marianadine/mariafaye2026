65cc7f1618031072a2fff47b1455043d9238d16f
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2026-08-19 20:37:54 +0800

    Home | added first section of home page

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import './index.css'
 import App from './App.jsx'
 
 import '@fontsource/geist-sans/400.css';
+import '@fontsource/geist-sans/500.css';
+import '@fontsource/geist-sans/600.css';
 import '@fontsource/geist-sans/700.css';
 import '@fontsource/geist-sans/800.css';
 
 import '@fontsource/geist-mono/400.css';
+import '@fontsource/geist-mono/500.css';
+import '@fontsource/geist-mono/600.css';
 import '@fontsource/geist-mono/700.css';
 
 createRoot(document.getElementById('root')).render(
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,22 @@
+import stripImage from '../imgs/homepageimg.png';
+import '../styles/commonstyles.css';
+import '../styles/home.css';
+
 export default function Home() {
-    return <div>Home Page</div>;
+    return (
+        <div>
+            <div className='first-section'>
+                <h3 className='introText'>hey, it’s nadz ☆</h3>
+
+                <div className="image-wrapper">
+                    <img src={stripImage} alt="Film strip photos of Nadz" className="film-strip" />
+                </div>
+
+                <div className='two-column'>
+                    <p>maria nadine faye rufo</p>
+                    <code>an aspiring UI/UX Designer and QA Engineer with a passion for clean design and smooth user experiences.</code>
+                </div>
+            </div>
+        </div>
+    )
 }
