92b4e5429afcc0e0cb0d142d87856e68cfd7898a
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2026-08-19 21:00:52 +0800

    Home Page | add side quests section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,10 @@
 import stripImage from '../imgs/homepageimg.png';
+
+import kmc from '../imgs/book-kmc.png';
+import sm from '../imgs/book-sm.png';
+import jpcs from '../imgs/book-jpcs.png';
+import umak from '../imgs/book-umak.png';
+
 import '../styles/commonstyles.css';
 import '../styles/home.css';
 
@@ -15,6 +21,18 @@ export default function Home() {
                 <div className='two-column'>
                     <p>maria nadine faye rufo</p>
                     <code>an aspiring UI/UX Designer and QA Engineer with a passion for clean design and smooth user experiences.</code>
+                </div>
+            </div>
+
+            <div className='second-section'>
+                <h3 className='section-name'>side quests</h3>
+                <code className='section-description'>not just work, but wins — the timeline of companies i have worked with, organizations, and competitions</code>
+
+                <div className="image-wrapper">
+                    <img src={sm} alt="SM Experience" className="book-card" />
+                    <img src={kmc} alt="KMC Experience" className="book-card" />
+                    <img src={jpcs} alt="JPCS Experience" className="book-card" />
+                    <img src={umak} alt="UMAK Experience" className="book-card" />
                 </div>
             </div>
         </div>
