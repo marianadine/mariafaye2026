fd4cb64f198960b5b5bb82b892ed781e7dc7716e
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2026-08-20 23:07:25 +0800

    Stack Page | Separate certicate data

--- src/pages/stack.jsx
+++ src/pages/stack.jsx
@@ -1,76 +1,16 @@
+import React from 'react';
 import TechStack from '../components/techStack';
+import { certifications } from '../data/stackData';
+
 import '../styles/commonstyles.css';
 import '../styles/stack.css';
 
-import htmlCssBadge from '../imgs/ITS-Badge-HTMLCSS.png';
-import jsBadge from '../imgs/ITS-Badge-JS.png';
-import simplilearnLogo from '../imgs/simplilearn_logo.png';
-import canvaLogo from '../imgs/canva_logo.jfif';
-
 export default function Stack() {
-  const certifications = [
-    {
-      id: 1,
-      issuer: 'Certiport',
-      title: 'Information Technology Specialist in HTML & CSS',
-      date: 'Issued Apr 2026 · Expires Apr 2031',
-      icon: htmlCssBadge,
-      link: 'https://www.credly.com/badges/982fd12c-9a23-4240-a9fe-7163b3e62dc6/public_url',
-    },
-    {
-      id: 2,
-      issuer: 'Certiport',
-      title: 'Information Technology Specialist in JavaScript',
-      date: 'Issued Apr 2026 · Expires Apr 2031',
-      icon: jsBadge,
-      link: 'https://www.credly.com/badges/dde8fd36-b3ca-4d7a-bbfe-05412bf3b312/public_url',
-    },
-    {
-      id: 3,
-      issuer: 'Simplilearn',
-      title: 'Introduction to Graphic Design; Basics of UI/UX',
-      date: 'Issued Oct 2025',
-      icon: simplilearnLogo,
-      link: 'https://www.simplilearn.com/skillup-certificate-landing?token=eyJjb3Vyc2VfaWQiOiIzNDA1IiwiY2VydGlmaWNhdGVfdXJsIjoiaHR0cHM6XC9cL2NlcnRpZmljYXRlcy5zaW1wbGljZG4ubmV0XC9zaGFyZVwvOTE0NjcyNF85NDYyOTc1MTc2MDE4MzU2MjU3My5wbmciLCJ1c2VybmFtZSI6Ik1hLiBOYWRpbmUgRmF5ZSBMLiBSdWZvIn0%3D&utm_source=shared-certificate&utm_medium=lms&utm_campaign=shared-certificate-promotion&referrer=https%3A%2F%2Flms.simplilearn.com%2Fcourses%2F6307%2FIntroduction%2520to%2520Graphic%2520Design%253B%2520Basics%2520of%2520UI%252FUX%2Fcertificate%2Fdownload-skillup&%24web_only=true&_branch_match_id=1618812142313736308&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXL87MLcjJ1EssKNDLyczL1k%2FVj0gpcQ23LCjNi0iyrytKTUstKsrMS49PKsovL04tsvUBqkpN8cwDABPuPhFBAAAA',
-    },
-    {
-      id: 4,
-      issuer: 'Simplilearn',
-      title: 'Introduction to Figma',
-      date: 'Issued Oct 2025',
-      icon: simplilearnLogo,
-      link: 'https://www.simplilearn.com/skillup-certificate-landing?token=eyJjb3Vyc2VfaWQiOiI0MzcyIiwiY2VydGlmaWNhdGVfdXJsIjoiaHR0cHM6XC9cL2NlcnRpZmljYXRlcy5zaW1wbGljZG4ubmV0XC9zaGFyZVwvOTA5MzgyNF85NDYyOTc1MTc1OTU0NjY3NzM2Ny5wbmciLCJ1c2VybmFtZSI6Ik1hLiBOYWRpbmUgRmF5ZSBMLiBSdWZvIn0%3D&utm_source=shared-certificate&utm_medium=lms&utm_campaign=shared-certificate-promotion&referrer=https%3A%2F%2Flms.simplilearn.com%2Fcourses%2F7320%2FIntroduction-to-Figma-course%2Fcertificate%2Fdownload-skillup&%24web_only=true&_branch_match_id=1618812142313736308&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXL87MLcjJ1EssKNDLyczL1k%2FVrwot8M8M9kpLikiyrytKTUstKsrMS49PKsovL04tsvUBqkpN8cwDAE1PiRlBAAAA',
-    },
-    {
-      id: 5,
-      issuer: 'Canva',
-      title: 'Graphic Design Essentials',
-      date: 'Issued Oct 2025',
-      icon: canvaLogo,
-      link: 'https://www.canva.com/design-school/certification-award/f5c88d02-28b9-471c-b96b-37e0f2a6b14a',
-    },
-    {
-      id: 6,
-      issuer: 'Canva',
-      title: 'Marketing with Canva',
-      date: 'Issued Oct 2025',
-      icon: canvaLogo,
-      link: 'https://www.canva.com/design-school/certification-award/21c2ff47-47a0-4030-af38-0db7af4c3760',
-    },
-    {
-      id: 7,
-      issuer: 'Canva',
-      title: 'Canva Essentials',
-      date: 'Issued Oct 2025',
-      icon: canvaLogo,
-      link: 'https://www.canva.com/design-school/certification-award/13be6340-6550-4bc3-beb7-4c57e965e4b7',
-    },
-  ];
-
   return (
     <div>
-        <TechStack />
-    <section className="certifications-section">
+      <TechStack />
+
+      <section className="certifications-section">
         <h3 className="section-name">certifications</h3>
 
         <div className="certifications-grid">
@@ -94,8 +34,6 @@ export default function Stack() {
           ))}
         </div>
       </section>
-
-
     </div>
   );
 }
